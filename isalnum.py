f3731363fdf3537d7f54f76df214c8cd806e7806
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-01-18 09:00:39 +0530

    Update isalnum.py

--- isalnum.py
+++ isalnum.py
@@ -1,5 +1,11 @@
 s=input()
-if s.isalnum():
-   print("Yes")
+if s.isalpha() or s.isdigit():
+  print("No")
+elif any(i.isalpha() for i in s):
+  if any(i.isdigit() for i in s):
+    print("Yes")
+  else:
+    print("No")    
 else:
-   print("No")
+   #print result
+  print("No")
